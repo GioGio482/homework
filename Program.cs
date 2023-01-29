fb99676cd6bd91fecc918e10823d174ccef9d7d8
Iluxa <iluxa705@yandex.ru>
2023-01-30 01:51:08 +0300

    HomeWork1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int a = 5;
+// int b = 7;
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// Console.Write(max);
+
+// int a = 2;
+// int b = 10;
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// Console.Write(max);
+
+// int a = -9;
+// int b = -3;
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// Console.Write(max);
+
+// int a = 2;
+// int b = 3;
+// int c = 7;
+
+// int max = a;
+
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+
+// Console.WriteLine(max);
+
+
+// int a = 44;
+// int b = 5;
+// int c = 78;
+
+// int max = a;
+
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+
+// Console.WriteLine(max);
+
+
+// int a = 22;
+// int b = 3;
+// int c = 9;
+
+// int max = a;
+
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+
+// Console.WriteLine(max);
+
+ 
+// int i;
+// Console.Write("Введите число : ");
+// i = int.Parse(Console.ReadLine());
+// if (i % 2 == 0)
+ 
+// {
+//     Console.Write("Введенное число является четным");
+//     Console.Read();
+// }
+// else
+// {
+//     Console.Write("Введенное число нечетное");
+//     Console.Read();
+// }
+
+
+
+// Console.Write("Введите число : ");
+// int.TryParse(Console.ReadLine(), out int n);
+// var i = 2;
+// while (i <= n)
+// {
+//     Console.Write(i + " ");
+//     i = i + 2;
+// }
