d76a2bad121490f44bc8cd353373493d818cedfd
Iluxa <iluxa705@yandex.ru>
2023-01-30 02:53:00 +0300

    Add Switch (needed rework)

--- Program.cs
+++ Program.cs
@@ -1,85 +1,133 @@
-﻿// int a = 5;
-// int b = 7;
-// int max = a;
-// if (a > max) max = a;
-// if (b > max) max = b;
-// Console.Write(max);
+﻿bool isWork = true;
+while (isWork)
+{
+    Console.WriteLine("Enter a number of task");
 
-// int a = 2;
-// int b = 10;
-// int max = a;
-// if (a > max) max = a;
-// if (b > max) max = b;
-// Console.Write(max);
+    if (int.TryParse(Console.ReadLine(), out int i))
+    {
+        switch (i)
+        {
+            case 1:
+            {
+                int a = 5;
+                int b = 7;
+                int max = a;
+                if (a > max) max = a;
+                if (b > max) max = b;
+                Console.Write(max);
+                break;
+            }
 
-// int a = -9;
-// int b = -3;
-// int max = a;
-// if (a > max) max = a;
-// if (b > max) max = b;
-// Console.Write(max);
+            case 2:
+            {
+                int a = 2;
+                int b = 10;
+                int max = a;
+                if (a > max) max = a;
+                if (b > max) max = b;
+                Console.Write(max);
+                break;
+            }
 
-// int a = 2;
-// int b = 3;
-// int c = 7;
+            case 3:
+            {
+                int a = -9;
+                int b = -3;
+                int max = a;
+                if (a > max) max = a;
+                if (b > max) max = b;
+                Console.Write(max);
+                break;
+            }
+        }
+        
+        switch (i)
+        {
+            case 1:
+            {
+                int a = 2;
+                int b = 3;
+                int c = 7;
 
-// int max = a;
+                int max = a;
 
-// if (a > max) max = a;
-// if (b > max) max = b;
-// if (c > max) max = c;
+                if (a > max) max = a;
+                if (b > max) max = b;
+                if (c > max) max = c;
 
-// Console.WriteLine(max);
+                Console.WriteLine(max);
+                break;
+            }
 
+            case 2:
+            {
+                int a = 44;
+                int b = 5;
+                int c = 78;
 
-// int a = 44;
-// int b = 5;
-// int c = 78;
+                int max = a;
 
-// int max = a;
+                if (a > max) max = a;
+                if (b > max) max = b;
+                if (c > max) max = c;
 
-// if (a > max) max = a;
-// if (b > max) max = b;
-// if (c > max) max = c;
+                Console.WriteLine(max);
+                break;
+            }
 
-// Console.WriteLine(max);
+            case 3:
+            {
+                int a = 22;
+                int b = 3;
+                int c = 9;
 
+                int max = a;
 
-// int a = 22;
-// int b = 3;
-// int c = 9;
+                if (a > max) max = a;
+                if (b > max) max = b;
+                if (c > max) max = c;
 
-// int max = a;
+                Console.WriteLine(max);
+                break;
+            }
+        }
 
-// if (a > max) max = a;
-// if (b > max) max = b;
-// if (c > max) max = c;
+        switch (i)
+            {}
+                int m;
+                Console.Write("Введите число : ");
+                m = int.Parse(Console.ReadLine());
+                if (m % 2 == 0)
+                
+                {
+                    Console.Write("Введенное число является четным");
+                    Console.Read();
+                }
+                else
+                {
+                    Console.Write("Введенное число нечетное");
+                    Console.Read();
+                    break;
+            }
 
-// Console.WriteLine(max);
+        switch (i)
+        {
+            {
+                Console.Write("Введите число : ");
+                int.TryParse(Console.ReadLine(), out int n);
+                var d = 2;
+                while (d <= n)
+                {
+                    Console.Write(i + " ");
+                    d = d + 2;
+                    break;
+                }
+            }
 
- 
-// int i;
-// Console.Write("Введите число : ");
-// i = int.Parse(Console.ReadLine());
-// if (i % 2 == 0)
- 
-// {
-//     Console.Write("Введенное число является четным");
-//     Console.Read();
-// }
-// else
-// {
-//     Console.Write("Введенное число нечетное");
-//     Console.Read();
-// }
-
-
-
-// Console.Write("Введите число : ");
-// int.TryParse(Console.ReadLine(), out int n);
-// var i = 2;
-// while (i <= n)
-// {
-//     Console.Write(i + " ");
-//     i = i + 2;
-// }
+            case -1:
+            {
+                isWork = false; break;
+            }
+        }
+    }
+}
